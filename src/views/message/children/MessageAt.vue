53d614cf568358c7e32a39a4923ed61b33b510cc
寻鹿 <3181716884@qq.com>
2023-12-07 01:06:44 +0800

    design: 设计了消息中心的整体布局

---
+++ src/views/message/children/MessageAt.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="message-at">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MessageAt",
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/message/children/MessageReply.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="message-reply">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MessageReply",
+}
+</script>
+
+<style scoped>
+
+</style>
